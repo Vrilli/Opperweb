82ea576cde9b6da342400d3dad94452326547943
vrilli <vrillyjcr@gmail.com>
2022-03-22 13:07:38 -0500

    listo

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouters from './router/AppRouters';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css'; import
-'bootstrap-css-only/css/bootstrap.min.css'; import
-'mdbreact/dist/css/mdb.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+// import 'mdbreact/dist/css/mdb.css';
+import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import "./styles/App.css"
 import { Provider } from 'react-redux';
 import { store } from './redux/store/store';
